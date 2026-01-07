451e00903b2b4a3e65e616d91c436b96a8c1c32c
portfolio <mayanknarnoliya41@gmail.com>
2026-01-07 14:43:13 +0530

    StudyByte Backend Only

---
+++ config/razorpay.js
@@ -0,0 +1,9 @@
+const Razorpay = require("razorpay");
+require("dotenv").config();
+
+console.log("Testing Razorpay Key:", process.env.RAZORPAY_KEY); // 👈 Ye check karo
+
+exports.instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY,
+    key_secret: process.env.RAZORPAY_SECRET,
+});
