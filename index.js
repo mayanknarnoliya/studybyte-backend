710e33d9438ccf5227a8fbc6704552d489c1c6ae
portfolio <mayanknarnoliya41@gmail.com>
2026-01-17 14:10:08 +0530

    update

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        // origin: 'https://study-notion-front-end-plum.vercel.app',
-        origin: "http://localhost:3000",
+        origin: 'https://study-notion-front-end-plum.vercel.app',
+//         origin: "http://localhost:3000",
         credentials: true,
         // Without credentials: true, cookies (such as session cookies) or other credentials would not be sent in cross-origin requests.
     })
